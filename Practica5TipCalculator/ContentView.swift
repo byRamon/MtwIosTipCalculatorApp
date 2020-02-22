535dc9706e0d590971cd9693ebaf34c512c1f294
ramon baeza yepez <By_ramon@outlook.com>
2020-02-22 12:05:13 -0600

    Initial Commit

---
+++ Practica5TipCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Practica5TipCalculator
+//
+//  Created by ByRamon on 22/02/20.
+//  Copyright © 2020 ByRamon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
