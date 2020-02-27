d69d9a165a8c1461368f005d18b66532790604b9
ramon baeza yepez <By_ramon@outlook.com>
2020-02-27 12:35:47 -0600

    done

--- Practica5TipCalculator/ContentView.swift
+++ Practica5TipCalculator/ContentView.swift
@@ -9,9 +9,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    @State private var totalCuenta = ""
+    @State private var noPersonas = 2
+    @State private var porcPropina = 2
+    let porcPropinas = [10,15,20,25,0]
+    var totalPorPersona: Double{
+        let nPersonas = Double(noPersonas + 2)
+        let pPropina = Double(porcPropinas[porcPropina])
+        let total = Double(totalCuenta) ?? 0
+        let totalPropina = total/100*pPropina
+        let tCuenta = total + totalPropina
+        let pPersona = tCuenta / nPersonas
+        return pPersona
     }
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Total de la cuenta", text: $totalCuenta).keyboardType(.decimalPad)
+                    Picker("Número de personas", selection: $noPersonas){
+                        ForEach(2 ..< 100){ index in
+                            Text("\(index) personas")
+                        }
+                    }
+                }
+                Section(header: Text("¿Cuanta propina deseas dar?")){
+                    Picker("Porcentaje propina", selection: $porcPropina){
+                        ForEach(0 ..< porcPropinas.count){
+                            Text("\(self.porcPropinas[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section{
+                    Text("$\(totalPorPersona, specifier: "%.2f")")
+                    //Text("$\(calcular())")
+                }
+            }.navigationBarTitle("Calculadora de Propinas")
+        }
+    }
+    /*
+    func calcular() -> Double{
+        let nPersonas = Double(noPersonas + 2)
+        let pPropina = Double(porcPropinas[porcPropina])
+        let total = Double(totalCuenta) ?? 0
+        let totalPropina = total/100*pPropina
+        let tCuenta = total + totalPropina
+        let pPersona = tCuenta / nPersonas
+        return pPersona
+    }*/
 }
 
 struct ContentView_Previews: PreviewProvider {
